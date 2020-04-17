3aef64f688cb8805be4c785a004b74c5695584e0
Andy Rusu <rusu.andy1@gmail.com>
2020-04-17 12:39:28 +0300

    Added auth components

--- src/frontend/finbuddy/components/auth.cljs
+++ src/frontend/finbuddy/components/auth.cljs
@@ -1,13 +1,195 @@
-(ns finbuddy.components.auth)
+(ns finbuddy.components.auth
+  (:require
+   [goog.dom.dataset :as gdata]
+   [goog.dom.forms :as gform]
+   [finbuddy.db :as db]
+   [finbuddy.auth :refer [handle-login]]
+   [finbuddy.components.app :refer [notification]]))
+
+(defn handle-link
+  [event]
+  (.preventDefault event)
+  (-> event
+      (.-currentTarget)
+      (gdata/get "page")
+      (keyword)
+      (db/set-page!)))
+
+(defn login-link
+  []
+  [:a {:href "#" :on-click handle-link :data-page :login} "Login"])
+
+(defn signup-link
+  []
+  [:a {:href "#" :on-click handle-link :data-page :signup} "Signup"])
+
+(defn forgot-link
+  []
+  [:a {:href "#" :on-click handle-link :data-page :forgot} "Forgot password"])
+
+(defn logout-link
+  []
+  [])
+
+(defn layout
+  [page]
+  [:section.hero.is-primary.is-fullheight
+   [:div.hero-body
+    [:div.container
+     [:div.columns.is-centered
+      [:div.column.is-5-tablet.is-4-desktop.is-3-widescreen
+       page]]]]])
+
+(defn field
+  [label name type key]
+  (let [value (get-in @db/content [:form key :value] nil)
+        error (get-in @db/content [:form key :error] false)]
+    [:div.field
+     [:label.label {:for name} label]
+     [:div.control.has-icons-left.has-icons-right
+      [:input
+       {:required "required"
+        :class (if error
+                 "input is-danger"
+                 (if value
+                   "input is-success"
+                   "input"))
+        :name name
+        :placeholder "e.g. bobsmith@gmail.com"
+        :type type
+        :value value
+        :onChange #(swap!
+                    db/content
+                    assoc-in
+                    [:form key :value]
+                    (gform/getValue (.-currentTarget %)))}]
+      [:span.icon.is-small.is-left [:i.fa.fa-envelope]]
+      (if error
+        [:span.icon.is-small.is-right [:i.fa.fa-exclamation-triangle]]
+        (when value [:span.icon.is-small.is-right [:i.fa.fa-check]]))
+      (when error [:p.help.is-danger error])]]))
+
+(defn check-field
+  [label name key]
+  [:div.field
+   [:label.checkbox
+    {:for name}
+    [:input {:type "checkbox"
+             :name name
+             :checked (get-in @db/content [:form key :value] false)
+             :onChange #(swap!
+                         db/content
+                         assoc-in
+                         [:form key :value]
+                         (not (boolean (get-in @db/content [:form key :value]))))}]
+    label]])
 
 (defn login
   []
-  [:h1 "login"])
+  [layout 
+   [:div.box
+    [:h1.title.has-text-dark.has-text-centered "Login"]
+    [:div.is-divider]
+    [:a.button.is-fullwidth.is-google
+     [:span.icon [:i.fab.fa-google]]
+     [:span "Google"]]
+    [:br]
+    [:a.button.is-fullwidth.is-facebook
+     [:span.icon [:i.fab.fa-facebook]]
+     [:span "Facebook"]]
+    [:br]
+    [:a.button.is-fullwidth.is-microsoft
+     [:span.icon [:i.fab.fa-microsoft]]
+     [:span "Microsoft"]]
+    [:div.is-divider {:data-content "OR"}]
+    [:form#login
+     {:action ""}
+     [field "Email" "email" "email" :email]
+     [field "Password" "password" "password" :password]
+     [check-field "Remember me" "remeber" :remember]
+     [:div.field
+      [:button.button.is-success
+       {:on-click handle-login}
+       "Login"]]]
+    [:div.is-divider {:data-content "OR"}]
+    [signup-link]
+    " | "
+    [forgot-link]]])
 
 (defn signup
   []
-  [:h1 "signup"])
+  [layout
+   [:div.box
+    [:h1.title.has-text-dark.has-text-centered "Signup"]
+    [:div.is-divider]
+    [:a.button.is-fullwidth.is-google
+     [:span.icon [:i.fab.fa-google]]
+     [:span "Google"]]
+    [:br]
+    [:a.button.is-fullwidth.is-facebook
+     [:span.icon [:i.fab.fa-facebook]]
+     [:span "Facebook"]]
+    [:br]
+    [:a.button.is-fullwidth.is-microsoft
+     [:span.icon [:i.fab.fa-microsoft]]
+     [:span "Microsoft"]]
+    [:div.is-divider {:data-content "OR"}]
+    [:form#signup
+     {:action ""}
+     [:div.field
+      [:label.label {:for "email"} "Email"]
+      [:div.control.has-icons-left
+       [:input.input
+        {:required "required"
+         :name "email"
+         :placeholder "e.g. bobsmith@gmail.com"
+         :type "email"}]
+       [:span.icon.is-small.is-left [:i.fa.fa-envelope]]]]
+     [:div.field
+      [:label.label {:for "password"} "Password"]
+      [:div.control.has-icons-left
+       [:input.input
+        {:required "required"
+         :name "password"
+         :placeholder "*******"
+         :type "password"}]
+       [:span.icon.is-small.is-left [:i.fa.fa-lock]]]]
+     [:div.field
+      [:label.label {:for "password-repeated"} "Repeat Password"]
+      [:div.control.has-icons-left
+       [:input.input
+        {:required "required"
+         :name "password-repeated"
+         :placeholder "*******"
+         :type "password"}]
+       [:span.icon.is-small.is-left [:i.fa.fa-lock]]]]
+     [:div.field
+      [:button.button.is-success
+       "Register"]]]
+    [:div.is-divider {:data-content "OR"}]
+    [login-link]]])
 
 (defn forgot
   []
-  [:h1 "forgot"])
+  [layout
+   [:div.box
+    [:h1.title.has-text-dark.has-text-centered "Forgot password"]
+    [:hr]
+    [:form
+     {:action ""}
+     [:div.field
+      [:label.label {:for "email"} "Email"]
+      [:div.control.has-icons-left
+       [:input.input
+        {:required "required"
+         :name "email"
+         :placeholder "e.g. bobsmith@gmail.com"
+         :type "email"}]
+       [:span.icon.is-small.is-left [:i.fa.fa-envelope]]]]
+     [:div.field
+      [:button.button.is-success
+       "Send Email"]]]
+    [:hr]
+    [signup-link]
+    " | "
+    [login-link]]])
