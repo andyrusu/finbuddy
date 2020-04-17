f7a91aab5087284547c324806d0aa6eba16d746f
Andy Rusu <rusu.andy1@gmail.com>
2020-04-17 12:39:10 +0300

    Added db functions for users

---
+++ src/frontend/finbuddy/db.cljs
@@ -0,0 +1,15 @@
+(ns finbuddy.db
+  (:require
+   [reagent.core :as r]))
+
+(def content (r/atom {:show-page :app
+                      :form nil
+                      :notifications []}))
+
+(defn set-page!
+  [page]
+  (swap! content #(assoc % :show-page page)))
+
+(defn set-form!
+  [data]
+  (swap! content #(assoc % :form data)))
